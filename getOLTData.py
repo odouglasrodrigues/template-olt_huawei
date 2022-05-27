a979d78991a11cb68f4d3d349c871fcfebefca60
Douglas Rodrigues <douglas.rodrigues96.dr@gmail.com>
2022-05-27 20:07:12 -0300

    start: Função para coletar media de sinal

--- getOLTData.py
+++ getOLTData.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
+from operator import le
 import sys
 import time
 import telnetlib
 import json
+import re
 
 
 def getOLTData(ip, user, password, port):
     count = 0
     placas = []
+    data = []
     pons = []
+    a_sinal=[]
     DataReturn = {
         'status': "sucesso",
         'consulta': "sucesso",
@@ -58,17 +62,52 @@ def getOLTData(ip, user, password, port):
         for linha in board_return:
             if "port 0/" in linha:
                 srt_pon = linha.split(',')
-                pon = srt_pon[0].split('port')[1].lstrip().rstrip('\r')
+                pon = srt_pon[0].split('port')[1].lstrip().rstrip(
+                    '\r').replace(' ', '')
                 onu_provisionada = int(
                     srt_pon[1].split(':')[1].lstrip().rstrip('\r'))
                 onu_online = int(
                     srt_pon[2].split(':')[1].lstrip().rstrip('\r'))
-                pons.append({
-                    pon:{
-                    "onuProvisionada": onu_provisionada,
-                    "onuOnline": onu_online
-                }})
-                print(json.dumps(pons))
+                data.append({
+                    pon: {
+                        "onuProvisionada": onu_provisionada,
+                        "onuOnline": onu_online,
+                        "bestSinal": '',
+                        "poorSinal": '',
+                        "mediaSinal": ''
+                    }
+                })
+                pons.append(pon)
+
+    for pon in pons:
+        f = pon.split('/')[0]
+        s = pon.split('/')[1]
+        p = pon.split('/')[2]
+        tn.write("interface gpon {}/{}\n".format(f, s))
+        time.sleep(.3)
+        tn.write("display ont optical-info {} all\n".format(p))
+        sinalList_return = tn.read_until('Control flag'.encode('utf-8'),
+        
+                                         3).decode('utf-8').splitlines()
+        print(pon)
+        for linha in sinalList_return:
+            
+            if re.search(r'.*-[0-9]+\.[0-9]+', linha):
+                srt_sinal=float(linha.split('-')[1].split(' ')[0])
+                a_sinal.append(srt_sinal)
+            if len(a_sinal)>0:
+                media=0
+                for sinal in a_sinal:
+                    media+sinal
+                media=(media/int(len(a_sinal)))
+                print(media)
+            a_sinal=[]    
+
+
+
+
+            
+
 
     # Fechando conexao com a OLT
     tn.write(b"quit\n")
